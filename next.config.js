cb9aefc0625100a5b005d4c66ded24a38049c6d3
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-03 04:28:59 -0500

    remove all Next.js files (to use Docz instead)

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/, // use MDX parsing for md & mdx files
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-})
